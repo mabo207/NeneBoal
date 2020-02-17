4f340ea2ca4daf3e60c32251cf8032f4ac51d533
mabo207 <mabo168general+github@gmail.com>
2020-02-17 19:54:34 +0900

    ボールの動きを調整した。
    - 徐々に減速するようになった。
    - 少しの傾きは無視するようにした。

--- src/Object/Boal.cpp
+++ src/Object/Boal.cpp
@@ -3,9 +3,22 @@
 #include"m5sensor.h"
 
 void Object::Boal::VelocityUpdate(){
+	// 加速度を加える
 	const double gravity = 16.0; // 重力加速度
-	m_vx += std::sin(M5Sensor::s_sensor.m_postureFilter.getPitchRadians()) * gravity;
-	m_vy += std::sin(M5Sensor::s_sensor.m_postureFilter.getRollRadians()) * gravity;
+	const float sinPitch = std::sin(M5Sensor::s_sensor.m_postureFilter.getPitchRadians());
+	const float sinRoll = std::sin(M5Sensor::s_sensor.m_postureFilter.getRollRadians());
+	if(sinPitch>=0.2 || sinPitch<=-0.2){
+		// 10度以下の傾きは無視
+		m_vx += sinPitch * gravity;
+	}
+	if(sinRoll>=0.2 || sinRoll<=-0.2){
+		// 10度以下の傾きは無視
+		m_vy += sinRoll * gravity;
+	}
+	// 徐々に減速するようにする(毎フレーム0.95倍する)
+	const int16_t multiple = 19, divide = 20;
+	m_vx = m_vx * multiple / divide;
+	m_vy = m_vy * multiple / divide;
 	// 場外に行かないようにする
 	if(GetX()+GetVX()<0){
 		m_vx = -GetX();
