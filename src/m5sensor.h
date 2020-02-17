9d2d5449b46fa231662d6065310327791981e9c3
mabo207 <mabo168general+github@gmail.com>
2020-02-17 16:21:41 +0900

    センサーの情報をまとめて管理するクラスを作成した
    - IMUを使ったコードの参照
    	- http://pages.switch-science.com/letsiot/rotateM5Stack/
    - ライブラリのインストールを参考にした
    	- https://qiita.com/watarukato/items/455c74c625e178de385d

---
+++ src/m5sensor.h
@@ -0,0 +1,23 @@
+#ifndef DEF_M5SENSOR_H
+#define DEF_M5SENSOR_H
+
+#include "utility\MPU9250.h" // ジャイロセンサー
+#include <MadgwickAHRS.h>
+
+// m5stackに内蔵or接続されているセンサーを管理する
+// 1つのm5stackに一定数しか存在しないので、static変数で管理しプログラム全体で利用できるようにする
+class M5Sensor{
+public:
+	M5Sensor();
+	~M5Sensor();
+	void Update();
+
+	MPU9250 m_mpu9250; // ジャイロセンサー
+	Madgwick m_postureFilter; // mpu9250を用いた姿勢情報を格納
+
+	// 静的変数
+	static void Init();
+	static M5Sensor s_sensor; // センサー群は1つのm5stackにつき1つだけ
+};
+
+#endif
