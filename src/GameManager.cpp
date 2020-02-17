fa87ac43724d367aa16dd5a7215b2d03910df2a1
mabo207 <mabo168general+github@gmail.com>
2020-02-17 17:37:49 +0900

    デバッグ情報を一部削除
    いい感じに操作できるようになった
    うまくいかなかった要因は2つ。
    - ボードにmpu9250のないm5stack Coreを指定していた
    - Wire.begin()をしていなかった。

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -16,28 +16,10 @@ void GameManager::Update(){
 
 void GameManager::Draw()const{
 	// オブジェクトを全消去
-	m5.Lcd.fillScreen(s_backColor);
+	m5.Lcd.fillRect(40, 160, 120, 60, s_backColor);
 	m_boal.BeforeErase();
 	// 描画処理
 	m_boal.Object::Base::Draw();
-	if (M5Sensor::s_sensor.m_mpu9250.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250) != 0x71){
-		m5.Lcd.drawString("cannnot find MPU9250", 20, 20);
-	}else{
-		m5.Lcd.drawString("can find MPU9250", 20, 20);
-	}
-	m5.Lcd.drawString("ax:", 40, 40);
-	m5.Lcd.drawNumber(M5Sensor::s_sensor.m_mpu9250.ax * 100, 100, 40);
-	m5.Lcd.drawString("ay:", 40, 60);
-	m5.Lcd.drawNumber(M5Sensor::s_sensor.m_mpu9250.ay * 100, 100, 60);
-	m5.Lcd.drawString("az:", 40, 80);
-	m5.Lcd.drawNumber(M5Sensor::s_sensor.m_mpu9250.az * 100, 100, 80);
-	m5.Lcd.drawString("gx:", 40, 100);
-	m5.Lcd.drawNumber(M5Sensor::s_sensor.m_mpu9250.gx * 100, 100, 100);
-	m5.Lcd.drawString("gy:", 40, 120);
-	m5.Lcd.drawNumber(M5Sensor::s_sensor.m_mpu9250.gy * 100, 100, 120);
-	m5.Lcd.drawString("gz:", 40, 140);
-	m5.Lcd.drawNumber(M5Sensor::s_sensor.m_mpu9250.gz * 100, 100, 140);
-
 	m5.Lcd.drawString("pitch:", 40, 160);
 	m5.Lcd.drawNumber(M5Sensor::s_sensor.m_postureFilter.getPitch() * 100, 100, 160);
 	m5.Lcd.drawString("roll:", 40, 180);
