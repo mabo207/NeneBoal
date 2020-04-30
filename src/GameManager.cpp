e3ae5586ff6477cb4f9071a49be44ff08615b8ca
mabo207 <mabo168general+github@gmail.com>
2020-04-30 17:18:57 +0900

    コメントを消した

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -26,7 +26,6 @@ void GameManager::Update()
 void GameManager::Draw() const
 {
 	// オブジェクトを全消去
-	// m5.Lcd.fillRect(40, 120, 160, 100, s_backColor);
 	m_boal.BeforeErase();
 	for (const Object::Terrain &terrain : m_terrainList)
 	{
@@ -39,14 +38,4 @@ void GameManager::Draw() const
 		terrain.Draw();
 	}
 	m_boal.Object::Base::Draw();
-	// m5.Lcd.drawString("X:", 40, 120);
-	// m5.Lcd.drawNumber(m_boal.GetX(), 100, 120);
-	// m5.Lcd.drawString("Y:", 40, 140);
-	// m5.Lcd.drawNumber(m_boal.GetY(), 100, 140);
-	// m5.Lcd.drawString("pitch:", 40, 160);
-	// m5.Lcd.drawNumber(M5Sensor::s_sensor.m_postureFilter.getPitch() * 100, 100, 160);
-	// m5.Lcd.drawString("roll:", 40, 180);
-	// m5.Lcd.drawNumber(M5Sensor::s_sensor.m_postureFilter.getRoll() * 100, 100, 180);
-	// m5.Lcd.drawString("yaw:", 40, 200);
-	// m5.Lcd.drawNumber(M5Sensor::s_sensor.m_postureFilter.getYaw() * 100, 100, 200);
 }
